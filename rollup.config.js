66b0992e02538e320abc395bd02c36595fde8fb1
Juan Fernández <juan@acyclic.co>
2023-02-09 19:03:42 +0100

    chore: dependencies and rollup settings updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,35 @@
-const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const resolve = require('rollup-plugin-node-resolve');
+const { babel } = require('@rollup/plugin-babel');
+const commonjs = require('@rollup/plugin-commonjs');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const external = require('rollup-plugin-peer-deps-external');
-const { terser } = require('rollup-plugin-terser');
 const typescript = require('@rollup/plugin-typescript');
+const replace = require('@rollup/plugin-replace');
+const { terser } = require('rollup-plugin-terser');
 
 const pkg = require('./package.json');
 
 const input = './src/index.js';
-const minifyExtension = (pathToFile) => pathToFile.replace(/\.js$/, '.min.js');
+const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, '.min.js');
 
 const external_namespace = 'WebsketchExt';
 
 module.exports = {
   input,
-  onwarn: function (message, next) {
+  onwarn: function(message, next) {
     console.error(message);
     process.exit(-1);
   },
   plugins: [
     babel({
-      //exclude: 'node_modules/**',
-      runtimeHelpers: true
+      exclude: 'node_modules/**',
+      babelHelpers: 'runtime'
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     external(),
-    resolve(),
+    nodeResolve(),
     commonjs(),
     typescript(),
     terser()
